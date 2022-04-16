23a9f1456df4df7e3469d5952a980a37b1d58fa6
Grosno <pbc20092yandex.ru>
2022-04-16 23:22:45 +0300

    Подключена библиотека recharts. Реализован шаблон гистограммы с мок данными

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import './App.scss';
 import { Button, Upload } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from 'recharts';
+import { data } from './mock/data';
 
 const App = () => {
   const handleUploadingFile = (info: any) => {
@@ -10,14 +20,30 @@ const App = () => {
 
   return (
     <div className="main">
-      <Upload
-        name="excel-file"
-        customRequest={handleUploadingFile}
-      >
-        <Button icon={<UploadOutlined />}>
-          Click to Upload
-        </Button>
-      </Upload>
+      <div>
+        <Upload
+          name="excel-file"
+          customRequest={handleUploadingFile}
+        >
+          <Button icon={<UploadOutlined />}>
+            Click to Upload
+          </Button>
+        </Upload>
+      </div>
+      <div>
+        <BarChart
+          width={1000}
+          height={600}
+          data={data}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="#8884d8" />
+        </BarChart>
+      </div>
     </div>
   );
 };
